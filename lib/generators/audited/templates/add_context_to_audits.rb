6939a0993d76c9e855d39cc14adb2d631cfd2a74
Andrei Makarov <andrei@kiskolabs.com>
2024-11-01 14:48:06 +0200

    Clean up add_context_to_audits upgrade migration

--- lib/generators/audited/templates/add_context_to_audits.rb
+++ lib/generators/audited/templates/add_context_to_audits.rb
@@ -1,12 +1,10 @@
-# frozen_string_literal: true
-
 <%- table_name = options[:audited_table_name].underscore.pluralize -%>
 class <%= migration_class_name %> < <%= migration_parent %>
-  def self.up
+  def up
     add_column :<%= table_name %>, :context, :jsonb
   end
 
-  def self.down
+  def down
     remove_column :<%= table_name %>, :context
   end
 end
